6eb2efa02e4f59e5dc051971ba44f1568de9550c
krrm <89742984+krrm1@users.noreply.github.com>
2022-10-16 14:03:33 +0300

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,7 +1,7 @@
 fx_version 'cerulean'
 game 'gta5'
 
-description 'qb-Table Made By Kareem Dacca arabcodingteam Send arabcodingteam'
+description 'qb-Table Made By Kareem Dacca krrm1'
 version '1.0.0'
 
 shared_script 'config.lua'
